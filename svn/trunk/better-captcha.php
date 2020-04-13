198910115f7f7ee08af52b5d5f8c31190d84cbc1
Rik Lewis <rik@rik.onl>
2020-04-13 23:40:18 +0100

    Added toggle in settings for login form captcha

--- svn/trunk/better-captcha.php
+++ svn/trunk/better-captcha.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:  Better Captcha
 Description:  Stop bad bots from attacking your forms using hCaptcha
-Version:      1.0
+Version:      1.1
 Author:       Better Security
 Author URI:   https://bettersecurity.co
 License:      GPL3
@@ -25,12 +25,16 @@ function better_capt_menus() {
 
 //add the settings
 function better_capt_settings() {
-	register_setting('better-captcha','better-captcha-settings');
+  register_setting('better-captcha','better-captcha-settings');
+  
 	add_settings_section('better-captcha-account', __('hCaptcha Account', 'better-capt-text'), 'better_capt_account', 'better-captcha');
 	add_settings_field('better-captcha-site-key', __('hCaptcha Site Key', 'better-capt-text'), 'better_capt_site_key', 'better-captcha', 'better-captcha-account');
 	add_settings_field('better-captcha-secret-key', __('hCaptcha Secret Key', 'better-capt-text'), 'better_capt_secret_key', 'better-captcha', 'better-captcha-account');
 	add_settings_field('better-captcha-theme', __('hCaptcha Theme', 'better-capt-text'), 'better_capt_theme', 'better-captcha', 'better-captcha-account');
-	add_settings_field('better-captcha-size', __('hCaptcha Size', 'better-capt-text'), 'better_capt_size', 'better-captcha', 'better-captcha-account');
+  add_settings_field('better-captcha-size', __('hCaptcha Size', 'better-capt-text'), 'better_capt_size', 'better-captcha', 'better-captcha-account');
+  
+  add_settings_section('better-captcha-places', __('Show Captcha', 'better-capt-text'), 'better_capt_places', 'better-captcha');
+	add_settings_field('better-captcha-place-login', __('Login Form', 'better-capt-text'), 'better_capt_place_login', 'better-captcha', 'better-captcha-places');  
 }
 
 //allow the settings to be stored
@@ -39,6 +43,7 @@ add_filter('whitelist_options', function($whitelist_options) {
   $whitelist_options['better-captcha'][] = 'better-captcha-secret-key';
   $whitelist_options['better-captcha'][] = 'better-captcha-theme';
   $whitelist_options['better-captcha'][] = 'better-captcha-size';
+  $whitelist_options['better-captcha'][] = 'better-captcha-place-login';
   return $whitelist_options;
 });
 
@@ -163,24 +168,54 @@ function better_capt_size() {
   echo '</select>';
 }
 
-function better_capt_create_option($def,$val,$rep,$boo) {
-  echo '  <option value="' . $val . '"' . ($def===$val ? ' selected' : '') . ($boo ? '' : ' disabled') . '>' . $rep . '</option>';
+//define output for settings section
+function better_capt_places() {
+  echo '<hr>';
+  echo '<p>Each form can be individually toggled to give you maximum control.</p>';
+}
+
+//defined output for settings
+function better_capt_place_login() {
+	$settings = get_option('better-captcha-settings');
+	$value = $settings['better-captcha-place-login'] ?: 'YES';
+  echo '<input name="better-captcha-settings[better-captcha-place-login]" type="hidden" value="NO">';
+  echo '<input id="better-captcha-place-login" name="better-captcha-settings[better-captcha-place-login]" type="checkbox" value="YES"' . ($value==='YES' ? ' checked' : '') . '>';
 }
 
 //add actions
 add_action('admin_menu','better_capt_menus');
 add_action('admin_init','better_capt_settings');
 
+//helper function for dropdowns
+function better_capt_create_option($def,$val,$rep,$boo) {
+  echo '  <option value="' . $val . '"' . ($def===$val ? ' selected' : '') . ($boo ? '' : ' disabled') . '>' . $rep . '</option>';
+}
+
 /*
---------------------- Display captcha challenges ---------------------
+----------------- Initalisation and Utilities -----------------
 */
+
+//initialisation
+function better_capt_init() {
+  $settings = get_option('better-captcha-settings');
+  $skey = $settings['better-captcha-site-key'] ?: '';
+  $sssh = $settings['better-captcha-secret-key'] ?: '';
+  if($skey!=='' && $sssh!=='') {
+    //login form
+    if(($settings['better-captcha-place-login'] ?: 'YES')==='YES') {
+      add_action('login_enqueue_scripts', 'better_capt_scripts');
+      add_filter('login_form', 'better_capt_display_login_form');
+      add_filter('wp_authenticate_user', 'better_capt_verify_login_form', 10, 2);
+    }
+  }
+}
+add_action('init', 'better_capt_init', 0);
 
 //include external script
 function better_capt_scripts() {
   wp_enqueue_script('hcaptcha-script', 'https://hcaptcha.com/1/api.js', array(), false, true);
 }
 add_action('wp_enqueue_scripts', 'better_capt_scripts');
-add_action('login_enqueue_scripts', 'better_capt_scripts');
 
 //display captcha challenge
 function better_capt_display_hcaptcha() {
@@ -202,7 +237,6 @@ function better_capt_display_login_form() {
     echo $capt . wp_nonce_field('better_captcha_login', 'better_captcha_nonce', true, false);
   }
 }
-add_filter('login_form', 'better_capt_display_login_form');
 
 function better_capt_verify_login_form($user, $password) {
   if(isset($_POST['better_captcha_nonce']) && wp_verify_nonce($_POST['better_captcha_nonce'],'better_captcha_login') && isset($_POST['h-captcha-response'])) {
@@ -221,7 +255,6 @@ function better_capt_verify_login_form($user, $password) {
   } 
   return new WP_Error(__("Captcha Invalid", 'better-capt-text'), __("<strong>ERROR</strong>: Invalid Captcha", 'better-capt-text'));   
 }
-add_filter('wp_authenticate_user', 'better_capt_verify_login_form', 10, 2);
 
 /*
 --------------------- Add links to plugins page ---------------------
